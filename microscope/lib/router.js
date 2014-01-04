7f911d68f6afd46b3b08183e2f934effc147253d
Randy L <randay@gmail.com>
2014-01-03 21:20:20 -0500

    this is working nicely, dynamic routing to individual posts

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -3,6 +3,13 @@ Router.configure({
 	loadingTemplate: 'loading',
 	waitOn: function() { return Meteor.subscribe('posts'); }
 });
+
 Router.map(function() {
 	this.route('postsList', {path: '/'});
+
+	this.route('postPage', {
+		path: '/posts/:_id',
+		// below sets up the "Data Context" for the postPage template
+		data: function() { return Posts.findOne(this.params._id); }
+	});
 });
