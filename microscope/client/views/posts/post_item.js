92a22907597aec337895d2d5390c501da27722a4
Randy L <randay@gmail.com>
2014-01-12 23:53:53 -0500

    Added edit posts form.

--- microscope/client/views/posts/post_item.js
+++ microscope/client/views/posts/post_item.js
@@ -1,4 +1,8 @@
 Template.postItem.helpers({
+	// SEE: post_item.html for where this helper func is used
+	ownPost: function() {
+		return this.userId == Meteor.userId();
+	},
 	domain: function() {
 		var a = document.createElement('a');
 		// notice that *this* is setup by the {{#each}} loop
