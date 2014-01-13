e7d7ac8f3d9712d552a94f437f00952392991b5f
Randy L <randay@gmail.com>
2014-01-12 20:10:31 -0500

    now must be logged in to submit a new post, removed insecure

--- microscope/collections/posts.js
+++ microscope/collections/posts.js
@@ -1,2 +1,8 @@
 // note: no `var` here means this variable is available everywhere
 Posts = new Meteor.Collection('posts');
+
+Posts.allow({
+	insert: function(userId, doc) {
+		return !! userId;
+	}
+})
