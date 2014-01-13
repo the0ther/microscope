8268649b79940e3795fc7ee76755d291800b1173
Randy L <randay@gmail.com>
2014-01-12 19:29:01 -0500

    added comments about imperative observe() data-binding
    there is no new code for the chapter on Reactivity

--- microscope/client/views/posts/posts_list.js
+++ microscope/client/views/posts/posts_list.js
@@ -1,3 +1,8 @@
+/* 
+This is the best way to make front-end talk to backend. nice and simple. but if this template were to talk to a 3rd party library (like GMaps) then you'd have to wire up an observe() call, and provide methods added(), removed(), changed().
+
+The code below is a demonstration of Meteor's "reactivity".
+*/
 Template.postsList.helpers({
 	posts: function() {
 		return Posts.find();
